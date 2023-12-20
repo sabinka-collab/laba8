8c0262cb1fbeb1e467937a261d83f79ac484eb18
sabyf <sabyf@SABA>
2023-12-21 00:19:13 +0300

    Добавьте файлы проекта.

---
+++ lab8/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите x");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y");
+int y = Convert.ToInt32(Console.ReadLine());
+try
+{
+    int z = x / y;
+    if (x == 0)
+    {
+        throw new Exception("Ошибка");
+    }
+    else
+    {
+        Console.WriteLine($"Ответ {z}");
+    }
+}
+catch (Exception j) when (x == 0)
+{
+Console.WriteLine($"Исключение {j.Message}");
+}
+catch (Exception r)
+{
+    Console.WriteLine(r.Message);
+}
